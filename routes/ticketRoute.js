55c019505756b435e7f138dc84660253448c5807
Albertosl2497 <146301297+Albertosl2497@users.noreply.github.com>
2024-01-02 20:06:43 -0700

    Update ticketRoute.js

--- routes/ticketRoute.js
+++ routes/ticketRoute.js
@@ -95,7 +95,7 @@ router.patch("/sell-tickets/:lotteryNo", async (req, res) => {
 
     const emailSubject = `CONFIRMACION DE APARTADO DE BOLETOS POR ${userInformation.fullName}`;
     const numTicketsPurchased = ticketNumbers.length; // Contar la cantidad de boletos comprados
-    const ticketPrice = 35; // Precio de cada boleto en pesos
+    const ticketPrice = 100; // Precio de cada boleto en pesos
 
     const totalCost = numTicketsPurchased * ticketPrice; // Calcular el costo total
 
@@ -103,10 +103,11 @@ router.patch("/sell-tickets/:lotteryNo", async (req, res) => {
     // Usamos la imagen "logo.png" en la misma carpeta
    const emailBody = `Hola,
     Quiero apartar ${numTicketsPurchased} boleto(s): [${ticketNumbers.join("] [")}].
-    Para la rifa de los $20,000 Pesos. El Domingo 31 de Diciembre 2023.
+    Para la rifa de los $7000 Pesos. El Domingo 7 de Enero 2024.
     El costo total a pagar es de $${totalCost} Pesos.
     Con el nombre de: ${userInformation.fullName}. 
-    Soy de: ${userInformation.city} ${userInformation.state} y mi número de teléfono es: ${userInformation.phoneNumber}.
+    Soy de: ${userInformation.city} ${userInformation.state}.
+    Mi número de teléfono es: ${userInformation.phoneNumber}.
 
     Gracias!
 
