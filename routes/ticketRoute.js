108baf5f691d489b13122a8c98a4dd7b9eb6270f
Albertosl2497 <146301297+Albertosl2497@users.noreply.github.com>
2024-01-04 22:06:28 -0700

    Actualizar ticketRoute.js

--- routes/ticketRoute.js
+++ routes/ticketRoute.js
@@ -103,11 +103,12 @@ router.patch("/sell-tickets/:lotteryNo", async (req, res) => {
     // Usamos la imagen "logo.png" en la misma carpeta
    const emailBody = `Hola,
     Quiero apartar ${numTicketsPurchased} boleto(s): [${ticketNumbers.join("] [")}].
-    Para la rifa de los $7000 Pesos. El Domingo 7 de Enero 2024.
-    El costo total a pagar es de $${totalCost} Pesos.
-    Con el nombre de: ${userInformation.fullName}. 
-    Soy de: ${userInformation.city} ${userInformation.state}.
-    Mi número de teléfono es: ${userInformation.phoneNumber}.
+    Para la rifa de los $7000 Pesos.
+De este Domingo 7 de Enero 2024.
+    》El costo total a pagar es de $${totalCost} Pesos.
+    ● Con el nombre de: ${userInformation.fullName}. 
+    ● Soy de: ${userInformation.city} ${userInformation.state}.
+    ● Mi número de teléfono es: ${userInformation.phoneNumber}.
 
     Gracias!
 
