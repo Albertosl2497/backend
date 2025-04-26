75d79f5e9875dcaa42678a0b6ea5731dd8b4cde4
Albertosl2497 <146301297+Albertosl2497@users.noreply.github.com>
2025-04-25 18:06:20 -0700

    Update emailService.js

--- services/emailService.js
+++ services/emailService.js
@@ -1,3 +1,5 @@
+const nodemailer = require("nodemailer");
+
 async function sendEmail(to, subject, text) {
   try {
     const transporter = nodemailer.createTransport({
@@ -9,7 +11,7 @@ async function sendEmail(to, subject, text) {
     });
 
     const mailOptions = {
-      from: "sorteosmg1@gmail.com",
+      from: "your_email_address",
       to,
       cc: "sorteosmg1@gmail.com",
       subject,
@@ -18,9 +20,11 @@ async function sendEmail(to, subject, text) {
 
     await transporter.sendMail(mailOptions);
 
-    console.log(`Email sent to ${to}`);
+    console.log(Email sent to ${to});
   } catch (error) {
-    //  Mostrá el error completo
-    console.error("ERROR AL ENVIAR:", error); 
+    console.error(Error sending email: ${error});
+    throw new Error("Failed to send email");
   }
 }
+
+module.exports = { sendEmail };
