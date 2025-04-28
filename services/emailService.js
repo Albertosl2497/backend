8fd9b8b9d6a55f2834df8dcae62163791548ee36
Albertosl2497 <146301297+Albertosl2497@users.noreply.github.com>
2025-04-28 12:32:00 -0700

    Update emailService.js

--- services/emailService.js
+++ services/emailService.js
@@ -20,9 +20,9 @@ async function sendEmail(to, subject, text) {
 
     await transporter.sendMail(mailOptions);
 
-    console.log(Email sent to ${to});
+    console.log(`Email sent to ${to}`);
   } catch (error) {
-    console.error(Error sending email: ${error});
+    console.error(`Error sending email: ${error}`);
     throw new Error("Failed to send email");
   }
 }
